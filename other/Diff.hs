550ec25a3b1290274f9493f53b97528d843614d7
Jordi Saludes <jordi.saludes@upc.edu>
2017-01-25 17:00:29 +0100

    Moved gdiff inspection down to Lit

--- other/Diff.hs
+++ other/Diff.hs
@@ -19,19 +19,19 @@ data TreeFamily :: * -> * -> * where
   ETyped' :: TreeFamily Tree (Cons Tree Nil) -- (Cons P.Type Nil))
   EFun' :: TreeFamily Tree (Cons CId  Nil)
   EApp' :: TreeFamily Tree (Cons Tree (Cons Tree Nil))
-  ELit' :: TreeFamily Tree Nil -- (Cons Literal Nil)
+  ELit' :: TreeFamily Tree (Cons Literal Nil)
   EMeta' :: TreeFamily Tree Nil -- (Cons MetaId Nil)
   EImplArg' :: TreeFamily Tree (Cons Tree Nil)
   --Explicit' :: TreeFamily BindType Nil
   --Implicit' :: TreeFamily BindType Nil
-  --Int'      :: Int -> TreeFamily Int Nil
-  --Double'   :: Double -> TreeFamily Double Nil
-  -- String'   :: String -> TreeFamily String Nil
+  Int'      :: Int -> TreeFamily Int Nil
+  Double'   :: Double -> TreeFamily Double Nil
+  String'   :: String -> TreeFamily String Nil
   CId'      :: TreeFamily CId (Cons BS.ByteString Nil)
   ByteString' :: BS.ByteString -> TreeFamily BS.ByteString Nil
-  --LStr' :: TreeFamily Literal (Cons String Nil)
-  --LInt' :: TreeFamily Literal (Cons Int Nil)
-  --LFlt' :: TreeFamily Literal (Cons Double Nil)
+  LStr' :: TreeFamily Literal (Cons String Nil)
+  LInt' :: TreeFamily Literal (Cons Int Nil)
+  LFlt' :: TreeFamily Literal (Cons Double Nil)
   EAbs' :: TreeFamily Tree (Cons Tree Nil)
   -- DTyp' :: TreeFamily P.Type (Cons [Hypo] (Cons CId (Cons [Tree] Nil)))
 
@@ -43,58 +43,59 @@ instance Family TreeFamily where
   decEq ELit'  ELit'  = Just (Refl, Refl)
   decEq EMeta' EMeta' = Just (Refl, Refl)
   decEq CId' CId'     = Just (Refl, Refl)
-  --decEq LStr' LStr'   = Just (Refl, Refl)
-  --decEq LInt' LInt'   = Just (Refl, Refl)
-  --decEq LFlt' LFlt'   = Just (Refl, Refl)
+  decEq LStr' LStr'   = Just (Refl, Refl)
+  decEq LInt' LInt'   = Just (Refl, Refl)
+  decEq LFlt' LFlt'   = Just (Refl, Refl)
   decEq EAbs'  EAbs'  = Just (Refl, Refl)
   decEq (ByteString' a) (ByteString' b) | a == b  = Just (Refl, Refl)
-    | otherwise                                  = Nothing
-  {- decEq (Int' x) (Int' y) | x == y     = Just (Refl, Refl)
-    | otherwise                       = Nothing
-   decEq (Double' x) (Double' y) | x == y     = Just (Refl, Refl)
-   | otherwise                       = Nothing
+                                        | otherwise                                  = Nothing
+  decEq (Int' x) (Int' y) | x == y     = Just (Refl, Refl)
+                          | otherwise             = Nothing
+  decEq (Double' x) (Double' y) | x == y     = Just (Refl, Refl)
+                                 | otherwise                       = Nothing
   decEq (String' a) (String' b) | a == b  = Just (Refl, Refl)
-    | otherwise = Nothing
-    -}
+                                | otherwise = Nothing
   decEq _      _      = Nothing
 
   fields EVar' (EVar i)     = Just CNil -- (CCons i CNil)
   fields EFun' (EFun c)  = Just (CCons c CNil)
   fields EApp' (EApp f x)  = Just (CCons f (CCons x CNil))
-  fields ELit' (ELit l)     = Just CNil -- (CCons l CNil)
+  fields ELit' (ELit l)     = Just (CCons l CNil)
   fields EMeta' (EMeta m)   = Just CNil -- (CCons m CNil)
   fields EAbs' (EAbs b c t) = Just (CCons t CNil)
   fields ETyped' (ETyped e t) = Just (CCons e CNil)
   fields EImplArg' (EImplArg e) = Just (CCons e CNil)
   --fields Explicit' _        = Just CNil
   --fields Implicit' _        = Just CNil
-  --fields (Int' _)  _        = Just CNil
-  --fields (Double' _)  _     = Just CNil
+  fields (String' _)  _     = Just CNil
+  fields (Int' _)  _        = Just CNil
+  fields (Double' _)  _     = Just CNil
   fields (ByteString' _)  _   = Just CNil
   fields CId' (CId bs)      =  Just (CCons bs CNil)
-  --fields LStr' (LStr s)     = Just (CCons s CNil)
-  --fields LInt' (LInt i)     = Just (CCons i CNil)
-  --fields LFlt' (LFlt x)     = Just (CCons x CNil)
+  fields LStr' (LStr s)     = Just (CCons s CNil)
+  fields LInt' (LInt i)     = Just (CCons i CNil)
+  fields LFlt' (LFlt x)     = Just (CCons x CNil)
   --fields f      args        = error $ "No field for " ++ string f
   fields _ _ = Nothing
 
   apply EVar' CNil                       = EVar 0
   apply EFun'  (CCons f CNil)            = EFun f
   apply EApp' (CCons f (CCons x CNil))   = EApp f x
-  apply ELit' CNil                       = ELit undefined
+  apply ELit' (CCons l CNil)             = ELit l
   apply ETyped' (CCons e CNil)           = ETyped e undefined
   apply EMeta' CNil                      = EMeta undefined
   apply EImplArg' (CCons e CNil)         = EImplArg e
   --apply Explicit' _                     = Explicit
   --apply Implicit' _                     = Implicit
-  --apply (Int' i) CNil                   = i
+  apply (Int' i) CNil                   = i
+  apply (String' s) CNil                = s
+  apply (Double' x) CNil                = x
   apply EAbs' (CCons t CNil) = EAbs undefined undefined t
   apply CId' (CCons c CNil)  = CId c
-  --apply (Double' x) CNil                = x
   apply (ByteString' bs) CNil             = bs
-  --apply LStr' (CCons s CNil)            = LStr s
-  --apply LInt' (CCons i CNil)            = LInt i
-  --apply LFlt' (CCons x CNil)            = LFlt x
+  apply LStr' (CCons s CNil)            = LStr s
+  apply LInt' (CCons i CNil)            = LInt i
+  apply LFlt' (CCons x CNil)            = LFlt x
 
   string EAbs' = "Abs"
   string EVar' = "Var"
@@ -105,6 +106,12 @@ instance Family TreeFamily where
   string EMeta' = "Meta"
   string CId'   = "CId"
   string (ByteString' bs) = "'" ++ BS.unpack bs
+  string LStr'        = "LStr"
+  string LInt'        = "LInt"
+  string LFlt'        = "LFlt"
+  string (Int' n)     = show n
+  string (Double' x)  = show x
+  string (String' s)  = show s
   string _ = undefined
 
 
@@ -118,19 +125,19 @@ instance Type TreeFamily CId where
 instance Type TreeFamily BS.ByteString where
   constructors = [Abstr ByteString']
 
-{-instance Type TreeFamily Int where
-  constructors = [Abstr Int']
-
-
-
 instance Type TreeFamily Literal where
   constructors = [Concr LStr', Concr LInt', Concr LFlt']
+
+instance Type TreeFamily Int where
+  constructors = [Abstr Int']
+
 instance Type TreeFamily Double where
   constructors = [Abstr Double']
 
 instance Type TreeFamily String where
   constructors = [Abstr String']
 
+{-
 instance Type TreeFamily BindType where
   constructors = [Concr Explicit', Concr Implicit']
 
